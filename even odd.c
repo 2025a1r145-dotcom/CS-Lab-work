4adeb7bebd3a35dcda40fbc8d5d35b92c7d7562b
2025a1r145-dotcom <2025a1r145@mietjammu.in>
2025-10-11 12:53:43 +0530

    Create even odd.c

---
+++ even odd.c
@@ -0,0 +1,15 @@
+to check whether a number is even or odd
+
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("enter the number:");
+    scanf("%d",&num);
+    if(num%2==0){
+            printf("%d is even",num);
+    }else {
+          printf("%d is odd",num);
+      }
+      return 0;
+    }
