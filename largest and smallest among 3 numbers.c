2121c874c0843a32d4e4a7bb0ecf096aae8e3934
2025a1r145-dotcom <2025a1r145@mietjammu.in>
2025-10-22 16:03:36 +0530

    Create largest and smallest among 3 numbers.c

---
+++ largest and smallest among 3 numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+  int a,b,c,largest,smallest;
+
+    printf("enter the numbers:");
+    scanf("%d %d %d",&a,&b,&c);
+
+    if(a>b && a>c)
+         largest=a;
+    else if (b>c)
+      largest=b;
+    else
+      largest=c;
+
+
+
+    if (a<b && a<c)
+        smallest=a;
+    else if (b<c)
+        smallest=b;
+    else
+        smallest=c;
+
+    printf("Largest=%d, Smallest=%d\n",largest,smallest);
+    return 0;
+}
